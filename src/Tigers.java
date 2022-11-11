fed8a56b2ca1f64dcd7d23dfea017308c946157e
yoon <beatus96@naver.com>
2022-11-11 15:50:22 +0900

    fix Tigers.java

--- src/Tigers.java
+++ src/Tigers.java
@@ -1,12 +1,7 @@
 public class Tigers {
 
 	String team = "타이거스";
-<<<<<<< HEAD
-	String manager = "데이비드";
-	String[] members = {"린다", "윌리엄","데이비드","조지"};
-=======
 	String manager = "존";
 	String[] members = {"린다", "윌리엄","데이비드"};
->>>>>>> parent of fc043dc (Replace Lions with Leopards)
 
 }
