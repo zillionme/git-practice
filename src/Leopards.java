861288fc6870d5ef51995aeffcccb30d271647bc
yoon <beatus96@naver.com>
2022-11-11 15:45:09 +0900

    REVERT "Replace Lions with Leopards"

--- src/Leopards.java
+++
@@ -1,5 +0,0 @@
-public class Leopards {
-	String team = "레오파드";
-	String manager = "노라";
-	String[] members = {"린다", "윌리엄","데이비드"};
-}
---
+++ src/Lions.java
@@ -0,0 +1,6 @@
+public class Lions {
+	String team = "라이언즈";
+	String manager = "메리";
+	String[] members = {"토마스", "카렌","마거릿"};
+
+}
